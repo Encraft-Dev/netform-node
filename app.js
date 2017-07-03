d52ad8ca7f3442ac905eedc34cfa43bba9eda787
j <j@J.com>
2017-07-03 16:49:07 +0100

    Update routes

--- app.js
+++ app.js
@@ -2,41 +2,23 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-//var compression = require('compression')
-//var expressStaticGzip = require("express-static-gzip");
-//global.appRoot = path.resolve(__dirname);
 
-var index = require('./routes/index');
 var api = require('./routes/api');
-var results = require('./routes/results');
 var users = require('./routes/users')
-var live = require('./routes/live')
 var app = express();
 
-
-
-
-// view engine setup  //not used here
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 // compress all responses // do not use here..
 //app.use(compression())
-
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 
 //app.use('/', index);
-app.use('/api', api);
-app.use('/users', users);
-//app.use('/sim', express.static('public'));
-
+app.use('/api', api);//sim api
+app.use('/users', users);//users api
+//deal with sending results back
 app.get("/results/*", function (req, res, next) {
   //leave main settings file uncompressed because chrome and jquery $getJSON apears to have timing issues with gzip
   //pretty damn hacky if you ask me
