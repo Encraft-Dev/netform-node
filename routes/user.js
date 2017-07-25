c440460bd2d830b0072ffc53909029929e376a7a
rsrc1147 <jperry1147@gmail.com>
2017-07-25 16:22:00 +0100

    ...conversely it wants the dates to be strings...

--- routes/user.js
+++ routes/user.js
@@ -45,8 +45,8 @@ router.post('/add/test',function(req,res,next){
   res.header("Access-Control-Allow-Origin", "*");
   var schema = require(path.join(appDir,"data","Users","template.json"))
   var data = req.body;
-  data.arrivaldatetime = timef.getSimTimefromISOtime(data.HTMLarrivaldatetime).toString();;
-  data.departuredatetime = timef.getSimTimefromISOtime(data.HTMLdeparturedatetime).toString();;
+  data.arrivaldatetime = '"'+timef.getSimTimefromISOtime(data.HTMLarrivaldatetime)+'"';
+  data.departuredatetime = '"'+timef.getSimTimefromISOtime(data.HTMLdeparturedatetime)+'"';
   data.vehicleId *= 1;
   data.netformcharge *= 1;
   var ajv = new Ajv({allErrors: true});
